cffe85da017b4362f066bcdf9f29c49d0c66a599
nicksevod <nikitasevod0601@gmail.com>
2022-08-11 23:53:37 +0400

    add new mini-game brain-prime

---
+++ bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import starPrimeGame from '../src/games/prime.js';
+
+starPrimeGame();
---
+++ src/games/prime.js
@@ -0,0 +1,31 @@
+import startGame from '../index.js';
+
+const generateRandomNum = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+const isPrimeNum = (num) => {
+  if (num < 2) {
+    return false;
+  }
+
+  for (let div = 2; div <= Math.sqrt(num); div += 1) {
+    if (num % div === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const startPrimeGame = () => {
+  const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+  const getCorrectAnswer = () => {
+    const question = generateRandomNum(1, 30);
+    const correctAnswer = isPrimeNum(question) ? 'yes' : 'no';
+    return [question, correctAnswer];
+  };
+
+  startGame(rules, getCorrectAnswer);
+};
+
+export default startPrimeGame;
