b1d9cd700c017ceeaa2304ab9e25c84c486959e4
nicksevod <nikitasevod0601@gmail.com>
2022-08-11 23:18:41 +0400

    add new mini-game brain-calc

---
+++ src/games/calc.js
@@ -0,0 +1,35 @@
+// import readlineSync from 'readline-sync';
+import startGame from '../index.js';
+
+const generateRandomNum = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+const operators = ['+', '-', '*'];
+
+const calculate = (operand1, operand2, operator) => {
+  switch (operator) {
+    case '+':
+      return operand1 + operand2;
+    case '-':
+      return operand1 - operand2;
+    default:
+      return operand1 * operand2;
+  }
+};
+
+const startCalcGame = () => {
+  const rules = 'What is the result of the expression?';
+
+  const getCorrectAnswer = () => {
+    const operator = operators[generateRandomNum(0, 3)];
+    const firstNum = generateRandomNum(1, 20);
+    const secondNum = generateRandomNum(1, 20);
+    const question = `${firstNum} ${operator} ${secondNum}`;
+    const correctAnswer = String(calculate(firstNum, secondNum, operator));
+
+    return [question, correctAnswer];
+  };
+
+  startGame(rules, getCorrectAnswer);
+};
+
+export default startCalcGame;
