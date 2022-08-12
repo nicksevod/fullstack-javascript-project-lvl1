33c71e4552dff592c7188f04273139f24665e539
nicksevod <nikitasevod0601@gmail.com>
2022-08-12 23:02:36 +0400

    code refactoring

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import readlineSync from 'readline-sync';
 
 const startGame = (rules, getCorrectAnswer) => {
   console.log('Welcome to the Brain Games!');
+
   const userName = readlineSync.question('May I have your name? ');
+
   console.log(`Hello, ${userName}!`);
   console.log(rules);
 
